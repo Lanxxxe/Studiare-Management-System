18674c103da73f59e52ae040cc6549b9fa7c653c
RoyalJelly <earlhernani@gmail.com>
2024-12-29 11:27:32 +0800

    finished foundation of ui

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('', reservation_index, name='reservation_index'),
     path('home/', reservation_home, name='reservation_home'),
+    path('list/', reservation_list, name='reservation_list'),
+    path('transactions/', reservation_transaction, name='reservation_transcation'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -7,3 +7,12 @@ def reservation_index(request):
 
 def reservation_home(request):
     return render(request, 'reserv_home.html')
+
+def reservation_list(request):
+    context = {
+        'range_value': range(9)  # Replace 3 with your desired number of rectangles
+    }
+    return render(request, 'reservation_list.html', context)
+
+def reservation_transaction(request):
+    return render(request, 'reservation_transactions.html')
