3bc7195a35a40bcc02c73f9a070df804a58d85ad
Lanxxxe <Lanxxxe>
2025-01-04 01:34:57 +0800

    Add reservation function for the users

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Reservation
 
+class ReservationAdmin(admin.ModelAdmin):
+    # Make the user_id field readonly
+    readonly_fields = ('reservation_id', 'user', 'space',)
 # Register your models here.
+admin.site.register(Reservation, ReservationAdmin)
