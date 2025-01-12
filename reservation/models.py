d03a3d7ac8c4af1cb4a8e742f2e46e73ed4c250f
RoyalJelly <earlhernani@gmail.com>
2025-01-12 15:19:32 +0800

    changes to landing page ui

--- reservation/models.py
+++ reservation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from management.models import HubSpaces
+from datetime import datetime
 
 class Reservation(models.Model):
     RESERVATION_STATUS = [
--- users/models.py
+++ users/models.py
@@ -12,23 +12,3 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback from {self.user.username} at {self.created_at}"
-    
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    firstname = models.CharField(max_length=20)
-    lastname = models.CharField(max_length=20)
-    username = models.CharField(max_length=20, unique=True)
-    user_email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"{self.firstname} {self.lastname} ({'Active' if self.is_active else 'Inactive'})"
-
-
-class UserEmail(models.Model):
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    user_email = models.EmailField()
-
-    def __str__(self):
-        return f"{self.user_id.username}'s Email: {self.user_email}"
